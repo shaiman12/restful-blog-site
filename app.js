63f11d1a6b53f9aa20cb0c473192ade2b386c8df
Shai Aarons <shai.aarons21@gmail.com>
2020-04-24 11:29:10 +0200

    Added the SHOW route

--- app.js
+++ app.js
@@ -58,6 +58,17 @@ app.post("/blogs", function(req, res){
 
 });
 
+//SHOW ROUTE
+app.get("/blogs/:id",function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs")
+        }else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
 app.listen(3000, process.env.IP, function(){
     console.log("SERVER IS RUNNING!");
 });
