9d48820ef187279be8c1455601b8105fd413b37f
Shai Aarons <shai.aarons21@gmail.com>
2020-04-24 10:41:35 +0200

    NEW route complete

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/",function(req,res){
     res.redirect("/blogs");
 });
 
-
+//INDEX ROUTE
 app.get("/blogs",function(req,res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -40,7 +40,23 @@ app.get("/blogs",function(req,res){
 
 });
 
+// NEW ROUTE
+app.get("/blogs/new",function(req,res){
+    res.render("new");
+});
 
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(error, newBlog){
+        if(error){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+
+    });
+
+});
 
 app.listen(3000, process.env.IP, function(){
     console.log("SERVER IS RUNNING!");
