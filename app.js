61a0db7424495440b28e7536ce9ed7b90f61af23
Shai Aarons <shai.aarons21@gmail.com>
2020-04-24 12:42:40 +0200

    Styled index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express             = require("express"),
     app                 = express(),
     methodOverride      = require("method-override"),
+    expressSanitizer    = require("express-sanitizer"),
     bodyParser          = require("body-parser"),
     mongoose            = require("mongoose");
 
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser: 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //Mongoose/Model Config
@@ -49,6 +51,7 @@ app.get("/blogs/new",function(req,res){
 
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(error, newBlog){
         if(error){
             res.render("new");
@@ -85,6 +88,7 @@ app.get("/blogs/:id/edit",function(req,res){
 
 //UPDATE ROUTE
 app.put("/blogs/:id",function(req,res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
